0f2c5dab826922db0fdd31b2f26213323f962e00
ezzahra12 <blnfatima64@gmail.com>
2026-09-20 17:34:40 +0000

    feat: update transaction model

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -7,4 +7,43 @@ private double montant;
 private LocalDateTime date;
 private Compte compteSource;
 private Compte compteDestination;
+    public Transaction(String idTransaction, TypeTransaction type, double montant,
+                       LocalDateTime date, Compte compteSource, Compte compteDestination) {
+
+        this.idTransaction = idTransaction;
+        this.type = type;
+        this.montant = montant;
+        this.date = date;
+        this.compteSource = compteSource;
+        this.compteDestination = compteDestination;
+    }
+    public Compte getCompteDestination() {
+        return compteDestination;
+    }
+
+    public Compte getCompteSource() {
+        return compteSource;
+
+    }
+
+
+
+
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public String getIdTransaction() {
+        return idTransaction;
+    }
+
+    public TypeTransaction getType() {
+        return type;
+    }
+
 }
