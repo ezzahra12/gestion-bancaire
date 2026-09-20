d52eac1b2ab36aea06e1e64ed3f845ba9efe23d3
ezzahra12 <blnfatima64@gmail.com>
2026-09-20 17:34:15 +0000

    feat: update account model

--- src/main/java/model/Compte.java
+++ src/main/java/model/Compte.java
@@ -1,15 +1,44 @@
 package model;
 
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.ArrayList;
-
+import java.util.HashSet;
 
 public abstract class Compte {
+
     protected String numeroCompte;
     protected double solde;
-    protected TypeCompte typeCompte;
-    protected List<Transaction> historiqueTransactions;
+    protected String typeCompte;
+    protected HashSet<Transaction> historiqueTransactions = new HashSet<>();
+
+    private boolean actif = true;
+
+    public String getNumeroCompte() {
+        return numeroCompte;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+
+
+
+    public String getTypeCompte() {
+        return typeCompte;
+    }
+
+    public HashSet<Transaction> getHistoriqueTransactions() {
+        return historiqueTransactions;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
 }
-
-
