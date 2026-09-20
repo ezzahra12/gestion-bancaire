f914f69a6cb78aaeba96e7da59a7e17e6d95e102
ezzahra12 <blnfatima64@gmail.com>
2026-09-20 17:34:34 +0000

    feat: update manager model

--- src/main/java/model/Gestionnaire.java
+++ src/main/java/model/Gestionnaire.java
@@ -1,4 +1,21 @@
 package model;
 
-public class Gestionnaire {
+public class Gestionnaire extends Personne {
+
+    private int idGestionnaire;
+
+    public Gestionnaire(String nom, String prenom, String email,
+                        String motDePasse, int idGestionnaire) {
+
+        super(nom, prenom, email, motDePasse);
+        this.idGestionnaire = idGestionnaire;
+    }
+
+    public int getIdGestionnaire() {
+        return idGestionnaire;
+    }
+
+    public void setIdGestionnaire(int idGestionnaire) {
+        this.idGestionnaire = idGestionnaire;
+    }
 }
