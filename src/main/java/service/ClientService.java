1d5fabf0bd680a6a993ecdddbb2d621c0b4efc6b
ezzahra12 <blnfatima64@gmail.com>
2026-09-20 17:33:58 +0000

    feat: update client service

--- src/main/java/service/ClientService.java
+++ src/main/java/service/ClientService.java
@@ -1,32 +1,83 @@
 package service;
 
+import exceptions.MontantInvalideException;
 import model.Client;
 
 import java.util.HashMap;
 
 public class ClientService {
-    private int idClient=1;
-    private HashMap<String, Client> clients=new HashMap<>();
 
-    public Client Inscrire(String nom, String prenom, String email,
-                           String motDePasse){
-        Client cl=new Client(  nom,
+    private int idClient = 1;
+
+    private HashMap<String, Client> clients =
+            new HashMap<>();
+
+    // ================= INSCRIPTION =================
+
+    public Client Inscrire(
+            String nom,
+            String prenom,
+            String email,
+            String motDePasse
+    ) {
+
+        Client cl = new Client(
+                nom,
                 prenom,
                 email,
                 motDePasse,
-                idClient);
-        clients.put(email,cl);
+                idClient
+        );
+
+        clients.put(email, cl);
+
         idClient++;
-        System.out.println("Inscription réussie");
-        System.out.println("ID:"+ cl.getIdClient());
+
+        System.out.println(
+                "Inscription réussie"
+        );
+
+        System.out.println(
+                "ID : " + cl.getIdClient()
+        );
+
         return cl;
     }
 
-    public Client authentifier(String email,String motDePass){
-        Client client = clients.get(email);
-        if (client != null && client.getMotDePasse().equals(motDePass)) {
+    // ================= AUTHENTIFICATION =================
+
+    public Client authentifier(
+            String email,
+            String motDePass
+    ) {
+
+        Client client =
+                clients.get(email);
+
+        if (client != null
+                && client.getMotDePasse()
+                .equals(motDePass)) {
+
             return client;
         }
+
+        return null;
+    }
+
+    // ================= TROUVER CLIENT =================
+
+    public Client trouverParId(int idClient) {
+
+        for (Client client :
+                clients.values()) {
+
+            if (client.getIdClient()
+                    == idClient) {
+
+                return client;
+            }
+        }
+
         return null;
     }
 }
